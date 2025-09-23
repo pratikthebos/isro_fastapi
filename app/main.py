40e8b82e9bedbf366b53e8c1d936b1293987f9a3
pratikthebos <pratikvkambale@53gmail.com>
2025-09-23 22:39:05 +0530

    pratik

--- app/main.py
+++ app/main.py
@@ -1,10 +1,43 @@
+# from fastapi import FastAPI
+# from app.routes import predict
+
+# app = FastAPI(title="Rain Prediction API")
+
+# app.include_router(predict.router, prefix="/api", tags=["Prediction"])
+
+# @app.get("/")
+# def root():
+#     return {"message": "Rain Prediction API is running! ISRO PROJECT requirement "}
+
 from fastapi import FastAPI
-from app.routes import predict
+import httpx
+import asyncio
 
 app = FastAPI(title="Rain Prediction API")
 
-app.include_router(predict.router, prefix="/api", tags=["Prediction"])
+URL = "https://student-pratikn.onrender.com"
+INTERVAL = 30  # seconds
+
+async def ping_website():
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(URL)
+            if response.status_code == 200:
+                print("Website reloaded successfully")
+            else:
+                print(f"Error: {response.status_code} {response.reason_phrase}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+async def periodic_ping():
+    while True:
+        await ping_website()
+        await asyncio.sleep(INTERVAL)
+
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(periodic_ping())
 
 @app.get("/")
 def root():
-    return {"message": "Rain Prediction API is running! ISRO PROJECT requirement "}
+    return {"message": "Rain Prediction API is running! ISRO PROJECT requirement"}
